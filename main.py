70a715ce6ccf6e728180c4e3161083c9af4bf108
koalasloth <kawinleebkk@gmail.com>
2022-11-06 15:07:14 -0500

    added constants

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import numpy as np
+from constantData import ConstantData 
+
+#need to install pylance
 
 #print(np.random.randint(1,5))
+maxFuelFlow = 390 #ml/min
+TMax = 100 # Newtons from max fuel flow
+velocityExitMax = 1560 #kph
+exitExhaustTempMaxLB = 480 #deg 
+exitExhaustTempMaxUB = 720 #deg 
+pressureRatio_compressor = 2.9 
+airMassFlow = .23
+
